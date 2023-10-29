0a831689c5d4f8b9ea1e0eb663f846311a82bf49
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-29 23:00:06 +0530

    origin added

--- soundly/settings.py
+++ soundly/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'soundly.urls'
 # CSRF_TRUSTED_ORIGINS = ['https://test-mkcw.onrender.com']
 CORS_ALLOW_ALL_ORIGINS = True
