26aa058f32f54a54ef0d23c9eda1241eebb2dca0
root <root@ip-172-31-3-169.ap-south-1.compute.internal>
2023-11-25 06:20:29 +0000

    deploy aws

--- soundly/urls.py
+++ soundly/urls.py
@@ -1,11 +1,12 @@
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('accounts.urls')),
-     path('',include('music.urls')),
-    path('game/', include('game.urls'))
+    path('',include('music.urls')),
+    path('game/', include('game.urls')),
+  
 ]
