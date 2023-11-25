142664f2e19b13823509941a384bada3ad19533e
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-11-25 13:41:21 +0530

    bug fix

--- accounts/views.py
+++ accounts/views.py
@@ -197,7 +197,7 @@ class VerifyOtpView(APIView):
                 'message': 'OTP verified successfully',
                 'data': {
                     "access_token": access_token,
-                    "user": user_serializer.data
+                    "user": user_serializer.data,
                     "refresh_token": refresh_token}
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
