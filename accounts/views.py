f34e9c75d9ceb4f8e1dc177b4c111311f5354483
ilakshaygupt <99826011+ilakshaygupt@users.noreply.github.com>
2023-11-25 13:33:07 +0530

    sending user data on verification

--- accounts/views.py
+++ accounts/views.py
@@ -192,10 +192,12 @@ class VerifyOtpView(APIView):
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
+            user_serializer = UserProfileSerializer(user)
             return Response({
                 'message': 'OTP verified successfully',
                 'data': {
                     "access_token": access_token,
+                    "user": user_serializer.data
                     "refresh_token": refresh_token}
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
